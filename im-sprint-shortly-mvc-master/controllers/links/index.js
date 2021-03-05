bdea190b32443af36a7e4a0478186c17a79d7d33
ohveloper <ohveloper@gmail.com>
2021-03-05 23:41:07 +0900

    test

--- im-sprint-shortly-mvc-master/controllers/links/index.js
+++ im-sprint-shortly-mvc-master/controllers/links/index.js
@@ -1,8 +1,14 @@
+const utils = require("../../modules");
+const model = require("../../models/url");
+
 module.exports = {
   get: (req, res) => {
     res.send();
   },
-  post: (req, res) => {
-    res.send();
+  post: async (req, res) => {
+    const { url } = req.body; // {url} 로 받아야됨
+    utils.getUrlTitle(url, (err, title) => {
+      module.url.create;
+    });
   },
 };
